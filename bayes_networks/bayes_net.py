e88bf123f0a610c7155cc2eee5d67d3b88d85949
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-05-28 19:11:42 +0300

    Add typing annotations in BayesNet class

--- bayes_networks/bayes_net.py
+++ bayes_networks/bayes_net.py
@@ -1,17 +1,18 @@
+from typing import List, Optional, Union
 from bayes_networks.bayes_node import BayesNode
 
 class BayesNet:
     """Bayesian network containing only boolean-variable nodes."""
 
-    def __init__(self, node_specs=None):
+    def __init__(self, node_specs: Optional[List[Union[str, List[str], float, dict]]] = None):
         """Nodes must be ordered with parents before children."""
-        self.nodes = []
-        self.variables = []
+        self.nodes: List[BayesNode] = []
+        self.variables: List[str] = []
         node_specs = node_specs or []
         for node_spec in node_specs:
             self.add(node_spec)
 
-    def add(self, node_spec):
+    def add(self, node_spec: Union[str, List[str], float, dict]):
         """Add a node to the net. Its parents must already be in the
         net, and its variable must not."""
         node = BayesNode(*node_spec)
@@ -22,7 +23,7 @@ class BayesNet:
         for parent in node.parents:
             self.variable_node(parent).children.append(node)
 
-    def variable_node(self, var):
+    def variable_node(self, var: str) -> BayesNode:
         """Return the node for the variable named var.
         >>> burglary.variable_node('Burglary').variable
         'Burglary'"""
@@ -31,9 +32,9 @@ class BayesNet:
                 return n
         raise Exception("No such variable: {}".format(var))
 
-    def variable_values(self, var):
+    def variable_values(self, var: str) -> List[bool]:
         """Return the domain of var."""
         return [True, False]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'BayesNet({0!r})'.format(self.nodes)
