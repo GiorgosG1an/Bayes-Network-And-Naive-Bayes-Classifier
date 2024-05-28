68aaa6e95837cdcff40d19e81c480f45dcc0bde4
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-05-28 18:42:46 +0300

    Add main module

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Main module for the second project for AI.
+University: University of Peloponnese, Department of Informatics and Telecommunications
+Course: Artificial Intelligence
+Authors: Giannopoulos Georgios
+         Giannopoulos Ioannis
+
+Project Description: Implementation of Naive Bayes Classifier for the classification of the spam emails. 
+"""
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
