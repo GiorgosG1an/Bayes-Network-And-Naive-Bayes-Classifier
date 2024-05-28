50bb8e2d32fe757648018c2fbdbf1389cab85beb
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-05-28 19:05:55 +0300

    Add BayesNet class for representing a Bayesian network

---
+++ bayes_networks/bayes_net.py
@@ -0,0 +1,39 @@
+from bayes_networks.bayes_node import BayesNode
+
+class BayesNet:
+    """Bayesian network containing only boolean-variable nodes."""
+
+    def __init__(self, node_specs=None):
+        """Nodes must be ordered with parents before children."""
+        self.nodes = []
+        self.variables = []
+        node_specs = node_specs or []
+        for node_spec in node_specs:
+            self.add(node_spec)
+
+    def add(self, node_spec):
+        """Add a node to the net. Its parents must already be in the
+        net, and its variable must not."""
+        node = BayesNode(*node_spec)
+        assert node.variable not in self.variables
+        assert all((parent in self.variables) for parent in node.parents)
+        self.nodes.append(node)
+        self.variables.append(node.variable)
+        for parent in node.parents:
+            self.variable_node(parent).children.append(node)
+
+    def variable_node(self, var):
+        """Return the node for the variable named var.
+        >>> burglary.variable_node('Burglary').variable
+        'Burglary'"""
+        for n in self.nodes:
+            if n.variable == var:
+                return n
+        raise Exception("No such variable: {}".format(var))
+
+    def variable_values(self, var):
+        """Return the domain of var."""
+        return [True, False]
+
+    def __repr__(self):
+        return 'BayesNet({0!r})'.format(self.nodes)
