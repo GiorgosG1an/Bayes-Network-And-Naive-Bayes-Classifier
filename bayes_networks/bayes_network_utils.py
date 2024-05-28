76efd8dded77396d28f334ab14b07ccfd35622f7
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-05-28 19:19:20 +0300

    Add extend, enumerate_all, enumeration_ask functions as utils of bayes networks

---
+++ bayes_networks/bayes_network_utils.py
@@ -0,0 +1,37 @@
+from probability_distribution.probdist import ProbDist
+from bayes_networks.bayes_node import BayesNode
+from bayes_networks.bayes_net import BayesNet
+
+def extend(s, var, val):
+    """Copy dict s and extend it by setting var to val; return copy."""
+    return {**s, var: val}
+
+def enumerate_all(variables, e, bn):
+    """Return the sum of those entries in P(variables | e{others})
+    consistent with e, where P is the joint distribution represented
+    by bn, and e{others} means e restricted to bn's other variables
+    (the ones other than variables). Parents must precede children in variables."""
+    if not variables:
+        return 1.0
+    Y, rest = variables[0], variables[1:]
+    Ynode = bn.variable_node(Y)
+    if Y in e:
+        return Ynode.p(e[Y], e) * enumerate_all(rest, e, bn)
+    else:
+        return sum(Ynode.p(y, e) * enumerate_all(rest, extend(e, Y, y), bn)
+                   for y in bn.variable_values(Y))
+
+
+def enumeration_ask(X, e, bn):
+    """
+    [Figure 14.9]
+    Return the conditional probability distribution of variable X
+    given evidence e, from BayesNet bn.
+    >>> enumeration_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary
+    ...  ).show_approx()
+    'False: 0.716, True: 0.284'"""
+    assert X not in e, "Query variable must be distinct from evidence"
+    Q = ProbDist(X)
+    for xi in bn.variable_values(X):
+        Q[xi] = enumerate_all(bn.variables, extend(e, X, xi), bn)
+    return Q.normalize()
