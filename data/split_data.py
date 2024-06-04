8b37f631eae9ee6e29bda7cd193e02b0920ce9a9
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-06-04 14:46:31 +0300

    Delete commented code lines

--- data/split_data.py
+++ data/split_data.py
@@ -22,13 +22,7 @@ def split_data(emails: List, y: List, train_ratio=0.8, seed=13):
     np.random.seed(seed)
     N = len(emails)
     idx = np.random.permutation(N)
-    # split_point = int(train_ratio * N)
     
-    # emails_train = [emails[i] for i in idx[:split_point]]
-    # labels_train = [labels[i] for i in idx[:split_point]]
-    
-    # emails_test = [emails[i] for i in idx[split_point:]]
-    # labels_test = [labels[i] for i in idx[split_point:]]
     emails_train = [emails[idx[i]] for i in range(int(0.8*N))]
     y_train = [y[idx[i]] for i in range(int(0.8*N))]
 
