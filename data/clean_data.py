0539ce876b9506dc50946560d644b05c593359c4
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-06-04 13:23:52 +0300

    Add docstring in the functions of clean_data module

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,6 +1,19 @@
+"""
+This module contains functions to clean the data.
+"""
 import re
+from typing import List
 
-def clean_str(string):
+def clean_str(string: str) -> set:
+    """
+    Cleans the input string by removing special characters, punctuation, and extra spaces.
+    
+    Args:
+        string (str): The input string to be cleaned.
+    
+    Returns:
+        set: A set of cleaned words from the input string.
+    """
     string = re.sub(r"[^A-Za-z0-9!?]", " ", string)
     string = re.sub(r"\'s", " \'s", string)
     string = re.sub(r"\'ve", " \'ve", string)
@@ -16,6 +29,15 @@ def clean_str(string):
     string = re.sub(r"\s{2,}", " ", string)
     return set(string.strip().lower().split())
 
-def clean_emails(emails):
+def clean_emails(emails: List[str]) -> List[str]:
+    """
+    Cleans a list of emails by applying the clean_str function to each email.
+
+    Args:
+        emails (list): A list of email strings.
+
+    Returns:
+        list: A list of cleaned email strings.
+    """
     return [clean_str(email) for email in emails]
 
