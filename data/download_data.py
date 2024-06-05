d19f1eef0eb2b927732320dbdd4a84e8ff8cc04f
Georgios Giannopoulos <dit20039@go.uop.gr>
2024-06-05 19:36:56 +0300

    Add module description and docstrings for the function in download_data.py

--- data/download_data.py
+++ data/download_data.py
@@ -1,10 +1,36 @@
+"""
+Module to download and extract email dataset.
+
+This module contains functions to download a dataset from a given URL and extract the dataset from a tar file.
+The dataset is expected to be a collection of emails, each of which is labeled as either 'ham' or 'spam'.
+The emails and their corresponding labels are returned as two separate lists.
+"""
 import urllib.request
 import tarfile
 
 def download_dataset(url: str, filename: str):
+    """
+    This function downloads a dataset from the given URL and saves it to a file with the given filename.
+
+    Args:
+    - url (str): The URL of the dataset to download.
+    - filename (str): The name of the file to save the downloaded dataset to.
+    """
     urllib.request.urlretrieve(url, filename)
 
 def extract_dataset(tar_filename: str):
+    """
+    This function extracts a dataset from a tar file.
+    The dataset is expected to be a collection of emails, each of which is labeled as either 'ham' or 'spam'.
+    The emails and their corresponding labels are returned as two separate lists.
+
+    Parameters:
+    - tar_filename (str): The name of the tar file to extract the dataset from.
+
+    Returns:
+    - emails (list): A list of emails extracted from the dataset. Each email is represented as a string.
+    - y (list): A list of labels corresponding to the emails. Each label is either 'ham' or 'spam'.
+    """
     emails = []
     y = []
     with tarfile.open(tar_filename, "r:gz") as tar:
